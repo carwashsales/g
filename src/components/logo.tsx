df564dd25b7d0dcd914628fe9cde4a125c6be85d
RAYMOND KAWOOYA <raymondkawooya7@gmail.com>
2025-10-19 13:44:49 +0000

    use  logo.png in public ihave put as logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import { cn } from '@/lib/utils';
 
 type LogoProps = {
@@ -5,17 +6,13 @@ type LogoProps = {
 };
 
 const Logo = ({ className }: LogoProps) => (
-    <svg 
-      xmlns="http://www.w3.org/2000/svg" 
-      viewBox="0 0 200 200" 
-      className={cn('fill-current', className)}
-    >
-      <g>
-        <path fill="#8bc34a" d="M100 20 L30 80 L100 130 L170 80 z"></path>
-        <path fill="#4caf50" d="M30 80 L100 130 L100 180 L30 130 z"></path>
-        <path fill="#7cb342" d="M100 130 L170 80 L170 130 L100 180 z"></path>
-      </g>
-    </svg>
+  <Image
+    src="/logo.png"
+    alt="E & G Cleanest Facilities Ltd Logo"
+    width={200}
+    height={200}
+    className={cn(className)}
+  />
 );
 
 export default Logo;
